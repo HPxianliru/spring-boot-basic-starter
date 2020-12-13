74015d7ab002142986c81cfaeff2c3c7efc8ed80
“xianliru” <402701653@qq.com>
2020-12-13 17:17:31 +0800

    1 生成者 统一处理

--- src/main/java/com/xian/rabbit/template/AmqTemplateUtil.java
+++ src/main/java/com/xian/rabbit/template/AmqTemplateUtil.java
@@ -3,14 +3,11 @@ package com.xian.rabbit.template;
 import com.google.gson.Gson;
 import com.xian.rabbit.enums.RabbitMQEnums;
 import com.xian.rabbit.exception.BussinesException;
+import com.xian.rabbit.util.SpringContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 /**
  * @program:
@@ -18,24 +15,11 @@ import javax.annotation.PostConstruct;
  * @author: xlr
  * @create: 2019-03-22 14:53
  **/
-@Component
 @Slf4j
 public class AmqTemplateUtil {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
     private static final Gson GSON = new Gson();
-
-    public static AmqTemplateUtil amqTemplateUtil;
-
-
-    @PostConstruct
-    public void init(){
-        amqTemplateUtil = this;
-        amqTemplateUtil.rabbitTemplate = rabbitTemplate;
-    }
-
+    
     /**
      *
      * @param exchange
@@ -51,7 +35,7 @@ public class AmqTemplateUtil {
             context = GSON.toJson(object);
         }
         try{
-            amqTemplateUtil.rabbitTemplate.convertAndSend(exchange,routingKey,  context,messagePostProcessor,correlationData);
+            rabbitTemplate().convertAndSend(exchange,routingKey,  context,messagePostProcessor,correlationData);
         }catch (Exception e){
             log.error("队列发送失败：queue key:"+routingKey+"json 内容："+context +" 错误信息{}",e.getMessage());
             throw new BussinesException("rabbitTemplate 发送消息失败");
@@ -65,7 +49,7 @@ public class AmqTemplateUtil {
     public static void sendDeadLetterMes(RabbitMQEnums rabbitMQEnums, String json){
         try{
             RabbitMQEnums delayQueue = RabbitMQEnums.DELAY_QUEUE;
-            amqTemplateUtil.rabbitTemplate.convertAndSend(delayQueue.getExchange(),delayQueue.getRoutingKey(),  json);
+            rabbitTemplate().convertAndSend(delayQueue.getExchange(),delayQueue.getRoutingKey(),  json);
         }catch (Exception e){
             log.error("延迟队列发送失败 "+"json 内容："+json +" 错误信息{}",e.getMessage());
         }
@@ -77,7 +61,7 @@ public class AmqTemplateUtil {
      */
     public static void sendDelayMes(RabbitMQEnums rabbitMQEnums,String json,String expiration){
         try{
-            amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),  json,message -> {
+            rabbitTemplate().convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),  json,message -> {
                 //注意这里时间要是字符串形式
                 message.getMessageProperties().setExpiration(expiration);
                 return message;
@@ -95,7 +79,7 @@ public class AmqTemplateUtil {
      */
     public static void sendMes(RabbitMQEnums rabbitMQEnums, String json, CorrelationData correlationData){
         try{
-            amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),  json,correlationData);
+            rabbitTemplate().convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),  json,correlationData);
         }catch (Exception e){
             log.error("队列发送失败：queue key:"+rabbitMQEnums.getRoutingKey()+"json 内容："+json +" 错误信息{}",e.getMessage());
         }
@@ -108,7 +92,7 @@ public class AmqTemplateUtil {
      */
     public static void sendDirectMes(RabbitMQEnums rabbitMQEnums, String json){
         try{
-            amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getQueueName(),json);
+            rabbitTemplate().convertAndSend(rabbitMQEnums.getQueueName(),json);
         }catch (Exception e){
             log.error("Direct队列发送失败：queue :"+rabbitMQEnums.getQueueName()+"json 内容："+json +" 错误信息{}",e.getMessage());
         }
@@ -121,7 +105,7 @@ public class AmqTemplateUtil {
      * @param messagePostProcessor 自定义 请求头
      */
     public void convertAndSend(RabbitMQEnums rabbitMQEnums, final Object message, final MessagePostProcessor messagePostProcessor){
-        amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getExchange(), rabbitMQEnums.getRoutingKey(), message, messagePostProcessor,null);
+        rabbitTemplate().convertAndSend(rabbitMQEnums.getExchange(), rabbitMQEnums.getRoutingKey(), message, messagePostProcessor,null);
     }
 
     /**
@@ -132,7 +116,7 @@ public class AmqTemplateUtil {
      */
     public static void sendMes(RabbitMQEnums rabbitMQEnums, Object object, CorrelationData correlationData){
 
-        amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),object,correlationData);
+        rabbitTemplate().convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),object,correlationData);
     }
 
     /**
@@ -141,7 +125,7 @@ public class AmqTemplateUtil {
      * @param object
      */
     public static void sendMes(RabbitMQEnums rabbitMQEnums, Object object){
-        amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),object);
+        rabbitTemplate().convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),object);
     }
 
     /**
@@ -150,6 +134,14 @@ public class AmqTemplateUtil {
      * @param str
      */
     public static void sendMes(RabbitMQEnums rabbitMQEnums, String str){
-        amqTemplateUtil.rabbitTemplate.convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),str);
+        rabbitTemplate().convertAndSend(rabbitMQEnums.getExchange(),rabbitMQEnums.getRoutingKey(),str);
+    }
+
+    /**
+     * 获取rabbitTemplate 模板 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+     * @return
+     */
+    public static RabbitTemplate rabbitTemplate(){
+        return SpringContextHolder.getBean( RabbitTemplate.class );
     }
 }
